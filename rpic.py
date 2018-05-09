922f75bcf3418a119d2fc533e8bd3c49980e5bbb
Martin Uribe <clamytoe@users.noreply.github.com>
2018-05-09 06:51:11 -0500

    Ran black against the script

--- rpic.py
+++ rpic.py
@@ -23,21 +23,21 @@ class Wallhaven(object):
 
         NOTE: Can combine them
     """
-    SITE = 'wallhaven.cc'
-    IMG = 'wallpaper.jpg'
-    WALLS = 'wallpaper'
-    HOME = path.expanduser('~')
-    IMG_FOLDER = path.join(HOME, 'Pictures')
+    SITE = "wallhaven.cc"
+    IMG = "wallpaper.jpg"
+    WALLS = "wallpaper"
+    HOME = path.expanduser("~")
+    IMG_FOLDER = path.join(HOME, "Pictures")
     IMG_PATH = path.join(IMG_FOLDER, IMG)
     WALLPAPERS = path.join(IMG_FOLDER, WALLS)
 
-    def __init__(self, purity=100, categories=111, sorting='random', order='desc'):
+    def __init__(self, purity=100, categories=111, sorting="random", order="desc"):
         self.purity = purity
         self.categories = categories
         self.sorting = sorting
         self.order = order
         self.url = self.construct_url
-        self.img_path = '//wallpapers.wallhaven.cc/wallpapers/full/'
+        self.img_path = "//wallpapers.wallhaven.cc/wallpapers/full/"
         self.images = self.get_images()
         self.current = 0
         self.check_dir(self.IMG_FOLDER)
@@ -51,8 +51,7 @@ class Wallhaven(object):
     @property
     def construct_url(self):
         """Takes the properties that were set for the object and constructs a proper url with them"""
-        url = f'https://alpha.{self.SITE}/search?categories={self.categories}&purity={self.purity}&sorting=' \
-              f'{self.sorting}&order={self.order}'
+        url = f"https://alpha.{self.SITE}/search?categories={self.categories}&purity={self.purity}&sorting=" f"{self.sorting}&order={self.order}"
         return url
 
     @staticmethod
@@ -61,38 +60,38 @@ class Wallhaven(object):
         r = requests.get(url)
         html = r.text
 
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, "html.parser")
         return soup
 
     def get_images(self):
         """Extract the image urls from the soup object and creates a list of them"""
         imgs = []
         soup = self.get(self.url)
-        previews = soup.find_all('a', class_='preview')
+        previews = soup.find_all("a", class_="preview")
 
         for link in previews:
-            imgs.append(link['href'])
+            imgs.append(link["href"])
         return imgs
 
     def next(self):
         """Parses the full image url from the image page and downloads it"""
         if self.current <= len(self.images) - 1:
             soup = self.get(self.images[self.current])
-            img_tags = soup.find_all('img')
+            img_tags = soup.find_all("img")
 
             for img in img_tags:
-                if self.img_path in img['src']:
-                    src = 'https:' + img['src']
-                    alt = img['alt']
+                if self.img_path in img["src"]:
+                    src = "https:" + img["src"]
+                    alt = img["alt"]
 
                     # TODO: Figure out a pythonic way of downloading the image
-                    cmd = f'curl -s -o {self.IMG_PATH} {src}'
+                    cmd = f"curl -s -o {self.IMG_PATH} {src}"
                     system(cmd)
 
-                    print(f'Retrieved: {alt}')
-                    save = input('Would you like to backup the image (y/[n])? ')
-                    if 'y' in save.lower():
-                        img_name = src.split('/')[-1]
+                    print(f"Retrieved: {alt}")
+                    save = input("Would you like to backup the image (y/[n])? ")
+                    if "y" in save.lower():
+                        img_name = src.split("/")[-1]
                         wallpaper = path.join(self.WALLPAPERS, img_name)
                         copyfile(self.IMG_PATH, wallpaper)
 
@@ -113,12 +112,12 @@ def main():
     haven.next()
 
     while keepem_coming:
-        answer = input('Would you like a different image([y]/n)? ')
-        if 'n' in answer.lower():
+        answer = input("Would you like a different image([y]/n)? ")
+        if "n" in answer.lower():
             keepem_coming = False
         else:
             haven.next()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
